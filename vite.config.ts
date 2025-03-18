69c4e6b1e41a72fa627fbb15844168a89b4f1bef
septemberonfire <dat.a.a@yandex.ru>
2025-03-18 20:09:11 +0300

    change base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/auth-form/',
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
